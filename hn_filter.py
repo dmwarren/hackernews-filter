dcb7c06d147b1ba6569a8ef05fddbafefc1ea4aa
Derek Warren <warren@sfu.ca>
2014-11-08 14:21:00 -0800

    Add Python 3.4 compatibility
    Still works with 2.7. Also enabled SSL verification since HN is now all
    SSL, all the time.

--- hn_filter.py
+++ hn_filter.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 from hn_filter_core import get_stories, filter_stories
-from pprint import pprint
 
 BOLDON = "\033[1m"
 BOLDOFF = "\033[0m"
@@ -12,12 +11,10 @@ good_stories = filtered_stories['good']
 crap_stories = filtered_stories['crap']
 
 if __name__ == '__main__':
-	for story in filtered_stories['good']:
-		#pprint(filtered_stories['good'])
- 		print(BOLDON + story['title'] + BOLDOFF) 
- 		print("  " + story['link'])
-		print("")
-		
-	print("Good: " + str(len(good_stories)))
-	print("Crap: " + str(len(crap_stories)))
+    for story in filtered_stories['good']:
+        print(BOLDON + story['title'] + BOLDOFF)
+        print("  " + story['link'])
+        print("")
 
+print("Good: " + str(len(good_stories)))
+print("Crap: " + str(len(crap_stories)))
